31aa69ec63783c819d5ea0640e734a481a4c1e5a
muertinho <kunzdo02@gmail.com>
2023-09-12 16:59:34 +0200

    -

--- src/transcribe_app.py
+++ src/transcribe_app.py
@@ -121,7 +121,7 @@ def main():
                         st.session_state.result = result
                     
                     if transcribe_original:
-                        st.text_area("**Transcription**", st.session_state.result["transcription"], height=200)
+                        st.text_area("**Transcription**", st.session_state.result["transcription"], height=300)
 
 if __name__ == "__main__":
     main()
